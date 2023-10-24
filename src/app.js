90b767354b444b1f30cc17fe5e5d193377a3ac80
Laquinh <anderaginaga2@gmail.com>
2023-10-24 18:01:30 +0200

    add app.js and script 'dev' to run the app

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const morgan = require('morgan');
+
+//settings
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//middlewares
+app.use(morgan('dev'));
+
+//routes
+
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
