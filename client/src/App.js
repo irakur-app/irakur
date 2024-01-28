7475bed88f241b14091f66068a1c84d791f5551b
Laquinh <anderaginaga2@gmail.com>
2024-01-28 18:43:04 +0100

    Create AddLanguage page

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './Pages/Home';
 import { Languages } from './Pages/Languages';
+import { AddLanguage } from './Pages/AddLanguage';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/languages" element={<Languages />} />
+        <Route exact path="/languages" element={<Languages />} />
+        <Route exact path="/languages/add" element={<AddLanguage />} />
       </Routes>
     </BrowserRouter>
   );
