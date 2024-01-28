f11b7ce3fe23ab883e0ce4ae3c77fefd1b19ed0f
Laquinh <anderaginaga2@gmail.com>
2024-01-28 20:01:19 +0100

    Create AddText page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './Pages/Home';
 import { Texts } from './Pages/Texts';
+import { AddText } from './Pages/AddText';
 import { Languages } from './Pages/Languages';
 import { AddLanguage } from './Pages/AddLanguage';
 import { EditLanguage } from './Pages/EditLanguage';
@@ -13,6 +14,7 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/texts" element={<Texts />} />
+        <Route exact path="/texts/add" element={<AddText />} />
         <Route exact path="/languages" element={<Languages />} />
         <Route exact path="/languages/add" element={<AddLanguage />} />
         <Route exact path="/languages/edit/:id" element={<EditLanguage />} />
