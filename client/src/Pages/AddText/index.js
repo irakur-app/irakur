e8b35f048e1311fba62f492555c8348c309ed912
Laquin <anderaginaga2@gmail.com>
2024-01-29 14:44:10 +0100

    Add copyright notice to new files

--- client/src/Pages/AddText/index.js
+++ client/src/Pages/AddText/index.js
@@ -1,1 +1,7 @@
+/* 
+ * Lingua Immerse - Learn languages through immersion
+ * Copyright (C) 2023-2024 Ander "Laquin" Aginaga San Sebastián
+ * Licensed under version 3 of the GNU Affero General Public License
+ */
+
 export * from './AddText';
