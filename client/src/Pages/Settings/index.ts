f28698529c271035f890c59b80460d8d77c8948a
Ander Aginaga / Laquin <anderaginaga2@gmail.com>
2024-01-30 11:54:06 +0100

    Delete client/src/Pages directory
    This directory has been accidentally created.

--- client/src/Pages/Settings/index.ts
+++
@@ -1,7 +0,0 @@
-/* 
- * Irakur - Learn languages through immersion
- * Copyright (C) 2023-2024 Ander "Laquin" Aginaga San Sebastián
- * Licensed under version 3 of the GNU Affero General Public License
- */
-
-export * from './settings';
