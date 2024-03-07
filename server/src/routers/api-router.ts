b81f6afb228a19508d7213391e4d77b6ab7db115
Ander Aginaga / Laquin <anderaginaga2@gmail.com>
2024-03-07 20:23:59 +0100

    Use a single router
    Having multiple routers was just making the code more inconvenient. At least for the moment, a single router seems the most logical solution. Don't fix what isn't broken.

--- server/src/routers/api-router.ts
+++ server/src/routers/api-router.ts
@@ -5,14 +5,25 @@
  */
 
 import express from 'express';
-const apiRouter = express.Router();
 
-import { settingsRouter } from './settings-router';
-import { languagesRouter } from './languages-router';
-import { textsRouter } from './texts-router';
+import { LanguagesController } from '../controllers/languages-controller';
+import { TextsController } from '../controllers/texts-controller';
 
-apiRouter.use('/settings/', settingsRouter);
-apiRouter.use('/languages/', languagesRouter);
-apiRouter.use('/texts/', textsRouter);
+const languagesController = new LanguagesController();
+const textsController = new TextsController();
 
-export { apiRouter };
+const router = express.Router();
+
+router.get('/languages/', languagesController.renderLanguages.bind(languagesController));
+router.get('/languages/edit/:id', languagesController.renderEditLanguage.bind(languagesController));
+router.post('/languages/add', languagesController.addLanguage.bind(languagesController));
+router.post('/languages/delete', languagesController.deleteLanguage.bind(languagesController));
+router.post('/languages/edit', languagesController.editLanguage.bind(languagesController));
+
+router.get('/texts/', textsController.renderTexts.bind(textsController));
+router.get('/texts/edit/:id', textsController.renderEditText.bind(textsController));
+router.post('/texts/add', textsController.addText.bind(textsController));
+router.post('/texts/delete', textsController.deleteText.bind(textsController));
+router.post('/texts/edit', textsController.editText.bind(textsController));
+
+export { router };
