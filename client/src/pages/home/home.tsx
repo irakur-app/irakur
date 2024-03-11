5fe63ae20cd90f36f0547f0f864be129b03fe01a
Ander Aginaga / Laquin <anderaginaga2@gmail.com>
2024-03-11 15:29:47 +0100

    Redo Home
    It no longer fetches front-end data from the server's API.

--- client/src/pages/home/home.tsx
+++ client/src/pages/home/home.tsx
@@ -4,60 +4,19 @@
  * Licensed under version 3 of the GNU Affero General Public License
  */
 
-import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
-type ApiData = {
-	title: string;
-	links: {
-		name: string;
-		url: string;
-	}[];
-	languages: {
-		id: number;
-		name: string;
-	}[];
-	activeLanguage: {
-		id: number;
-	} | null;
-};
-
 const Home = () => {
-	const [apiData, setApiData] = useState<ApiData | null>(null);
-
-	useEffect(() => {
-		fetch('/api/home')
-			.then((response) => response.json())
-			.then((data) => setApiData(data))
-			.catch((error) => console.error('Error fetching data:', error));
-	}, []);
-
-	if (!apiData) {
-		return <p>Loading...</p>;
-	}
-
-	// Render your React components using the fetched data
 	return (
 		<HelmetProvider>
 			<Helmet>
-				<title>{apiData.title}</title>
+				<title>Irakur - Home</title>
 			</Helmet>
-			<h1>{apiData.title}</h1>
-			<h3>Active language:</h3>
-			<form id="activeLanguageForm" method="post">
-					<select id="activeLanguage" name="activeLanguage" defaultValue={apiData.activeLanguage?.id}>
-							<option value="" disabled hidden>Select language</option>
-							{apiData.languages.map((language, i) => (
-								<option value={language.id} key={language.id}>{language.name}</option>
-							))}
-					</select>
-			</form>
-			{apiData.links.map((link, i) => (
-				<React.Fragment key={i}>
-					<a href={link.url}>{link.name}</a>
-					<br />
-				</React.Fragment>
-			))}
+			<h1>Irakur - Home</h1>
+			<Link to="/languages">Go to languages</Link>
+			<br />
+			<Link to="/texts">Go to texts</Link>
 		</HelmetProvider>
 	);
 };
