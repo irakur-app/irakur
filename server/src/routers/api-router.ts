d7590b2c6fe3955f8cf5db387a56f1143896fec5
Ander Aginaga / Laquin <anderaginaga2@gmail.com>
2024-03-16 13:13:10 +0100

    Basic error handling for server

--- server/src/routers/api-router.ts
+++ server/src/routers/api-router.ts
@@ -18,22 +18,35 @@ const wordsController = new WordsController();
 
 const router = express.Router();
 
+const errorWrapper = (handler: (req: express.Request, res: express.Response) => Promise<void>) => {
+    return async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+        try
+		{
+            await handler(req, res);
+        }
+		catch (error)
+		{
+            res.sendStatus(500);
+        }
+    };
+};
+
 //#region Languages
 router.get(
 	'/languages/',
-	async (req:express.Request, res:express.Response) => {
+	errorWrapper(async (req:express.Request, res:express.Response) => {
 		res.json({languages: await languagesController.getAllLanguages()});
-	}
+	})
 );
 router.get(
 	'/languages/:languageId',
-	async (req:express.Request, res:express.Response) => {
+	errorWrapper(async (req:express.Request, res:express.Response) => {
 		res.json({language: await languagesController.getLanguage(parseInt(req.params.languageId))});
-	}
+	})
 );
 router.post(
 	'/languages/',
-	async (req:express.Request, res:express.Response) => {
+	errorWrapper(async (req:express.Request, res:express.Response) => {
 		if(await languagesController.addLanguage(req.body.name, req.body.dictionaryUrl, req.body.shouldShowSpaces))
 		{
 			res.sendStatus(200);
@@ -42,11 +55,11 @@ router.post(
 		{
 			res.sendStatus(500);
 		}
-	}
+	})
 );
 router.delete(
 	'/languages/:languageId',
-	async (req:express.Request, res:express.Response) => {
+	errorWrapper(async (req:express.Request, res:express.Response) => {
 		if(await languagesController.deleteLanguage(parseInt(req.params.languageId)))
 		{
 			res.sendStatus(200);
@@ -55,11 +68,11 @@ router.delete(
 		{
 			res.sendStatus(500);
 		}
-	}
+	})
 );
 router.patch(
 	'/languages/:languageId',
-	async (req:express.Request, res:express.Response) => {
+	errorWrapper(async (req:express.Request, res:express.Response) => {
 		if(await languagesController.editLanguage(parseInt(req.params.languageId), req.body.name, req.body.dictionaryUrl, req.body.shouldShowSpaces))
 		{
 			res.sendStatus(200);
@@ -68,14 +81,14 @@ router.patch(
 		{
 			res.sendStatus(500);
 		}
-	}
+	})
 );
 //#endregion
 
 //#region Texts
 router.get(
 	'/texts/',
-	async (req:express.Request, res:express.Response) => {
+	errorWrapper(async (req:express.Request, res:express.Response) => {
 		if(req.query.languageId !== undefined)
 		{
 			res.json({texts: await textsController.getTextsByLanguage(parseInt(req.query.languageId as string))});
@@ -84,22 +97,22 @@ router.get(
 		{
 			res.json({texts: await textsController.getAllTexts()});
 		}
-	}
+	})
 );
 router.get(
 	'/texts/:textId',
-	async (req:express.Request, res:express.Response) => {
+	errorWrapper(async (req:express.Request, res:express.Response) => {
 		res.json(
 			{
 				text: await textsController.getText(parseInt(req.params.textId)),
 				numberOfPages: await textsController.getNumberOfPages(parseInt(req.params.textId))
 			}
 		);
-	}
+	})
 );
 router.post(
 	'/texts/',
-	async (req:express.Request, res:express.Response) => {
+	errorWrapper(async (req:express.Request, res:express.Response) => {
 		if(await textsController.addText(req.body.languageId, req.body.title, req.body.content, req.body.sourceUrl, req.body.numberOfPages))
 		{
 			res.sendStatus(200);
@@ -108,11 +121,11 @@ router.post(
 		{
 			res.sendStatus(500);
 		}
-	}
+	})
 );
 router.delete(
 	'/texts/:textId',
-	async (req:express.Request, res:express.Response) => {
+	errorWrapper(async (req:express.Request, res:express.Response) => {
 		if(await textsController.deleteText(parseInt(req.params.textId)))
 		{
 			res.sendStatus(200);
@@ -121,11 +134,11 @@ router.delete(
 		{
 			res.sendStatus(500);
 		}
-	}
+	})
 );
 router.patch(
 	'/texts/:textId',
-	async (req:express.Request, res:express.Response) => {
+	errorWrapper(async (req:express.Request, res:express.Response) => {
 		if(await textsController.editText(req.body.languageId, req.body.title, req.body.sourceUrl, req.body.numberOfPages, req.body.content, parseInt(req.params.textId)))
 		{
 			res.sendStatus(200);
@@ -134,32 +147,32 @@ router.patch(
 		{
 			res.sendStatus(500);
 		}
-	}
+	})
 );
 //#endregion
 
 //#region Pages
 router.get(
 	'/texts/:textId/pages/',
-	async (req:express.Request, res:express.Response) => {
+	errorWrapper(async (req:express.Request, res:express.Response) => {
 		res.json({pages: await pagesController.getAllPages(parseInt(req.params.textId))});
-	}
+	})
 );
 router.get(
 	'/texts/:textId/pages/:pageId',
-	async (req:express.Request, res:express.Response) => {
+	errorWrapper(async (req:express.Request, res:express.Response) => {
 		res.json({page: await pagesController.getPage(parseInt(req.params.textId), parseInt(req.params.pageId))});
-	}
+	})
 );
 router.get(
 	'/texts/:textId/pages/:pageId/words',
-	async (req:express.Request, res:express.Response) => {
+	errorWrapper(async (req:express.Request, res:express.Response) => {
 		res.json({words: await pagesController.getWords(parseInt(req.params.textId), parseInt(req.params.pageId))});
-	}
+	})
 );
 router.patch(
 	'/texts/:textId/pages/:pageId',
-	async (req:express.Request, res:express.Response) => {
+	errorWrapper(async (req:express.Request, res:express.Response) => {
 		if(await pagesController.editPage(parseInt(req.params.textId), req.body.index, req.body.content, parseInt(req.params.pageId)))
 		{
 			res.sendStatus(200);
@@ -168,20 +181,20 @@ router.patch(
 		{
 			res.sendStatus(500);
 		}
-	}
+	})
 );
 //#endregion
 
 //#region Words
 router.get(
 	'/words/:wordId',
-	async (req:express.Request, res:express.Response) => {
+	errorWrapper(async (req:express.Request, res:express.Response) => {
 		res.json({word: await wordsController.getWord(parseInt(req.params.wordId))});
-	}
+	})
 );
 router.post(
 	'/words/',
-	async (req:express.Request, res:express.Response) => {
+	errorWrapper(async (req:express.Request, res:express.Response) => {
 		if(await wordsController.addWord(req.body.languageId, req.body.content, req.body.status, req.body.entries, req.body.notes, req.body.datetimeAdded, req.body.datetimeUpdated))
 		{
 			res.sendStatus(200);
@@ -190,11 +203,11 @@ router.post(
 		{
 			res.sendStatus(500);
 		}
-	}
+	})
 );
 router.delete(
 	'/words/:wordId',
-	async (req:express.Request, res:express.Response) => {
+	errorWrapper(async (req:express.Request, res:express.Response) => {
 		if(await wordsController.deleteWord(parseInt(req.params.wordId)))
 		{
 			res.sendStatus(200);
@@ -203,11 +216,11 @@ router.delete(
 		{
 			res.sendStatus(500);
 		}
-	}
+	})
 );
 router.patch(
 	'/words/:wordId',
-	async (req:express.Request, res:express.Response) => {
+	errorWrapper(async (req:express.Request, res:express.Response) => {
 		if(await wordsController.editWord(req.body.languageId, req.body.content, req.body.status, req.body.entries, req.body.notes, req.body.datetimeAdded, req.body.datetimeUpdated, parseInt(req.params.wordId)))
 		{
 			res.sendStatus(200);
@@ -216,7 +229,7 @@ router.patch(
 		{
 			res.sendStatus(500);
 		}
-	}
+	})
 );
 //#endregion
 
