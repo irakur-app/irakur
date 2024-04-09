2c88bd193be96d725fdc43a4089144be64fff509
Ander Aginaga / Laquin <anderaginaga2@gmail.com>
2024-04-09 20:21:04 +0200

    Add missing copyright notices

--- common/types.ts
+++ common/types.ts
@@ -1,3 +1,9 @@
+/*
+ * Irakur - Learn languages through immersion
+ * Copyright (C) 2023-2024 Ander "Laquin" Aginaga San Sebastián
+ * Licensed under version 3 of the GNU Affero General Public License
+ */
+
 type Language = {
 	id: number;
 	name: string;
