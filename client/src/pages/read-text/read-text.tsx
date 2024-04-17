204514438ddb2c69f05c3748551438d2c94e93e7
Ander Aginaga / Laquin <anderaginaga2@gmail.com>
2024-04-17 13:35:57 +0200

    Basic ReadText structure

--- client/src/pages/read-text/read-text.tsx
+++ client/src/pages/read-text/read-text.tsx
@@ -7,6 +7,7 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { Outlet } from 'react-router-dom';
 
+import { EditWord } from '../../components/edit-word';
 import { Reader } from '../../components/reader';
 
 const ReadText = (): JSX.Element => {
@@ -16,7 +17,29 @@ const ReadText = (): JSX.Element => {
 				<title>Irakur - Read</title>
 			</Helmet>
 
-			<Reader />
+			<div style={{ display: 'flex', margin: '5%' }}>
+				<div style={{
+					flex: 1,
+					width: '80%',
+					marginRight: '5%',
+					fontSize: '1.5em',
+					overflowY: 'scroll',
+					height: '80vh',
+					textAlign: 'justify',
+					paddingRight: '1%',
+				}}>
+					<Reader />
+				</div>
+				<div style={{
+					position: 'sticky',
+					backgroundColor: '#00000011',
+					borderRadius: '25px',
+					padding: '2%',
+					width: '20%',
+				}}>
+					<EditWord />
+				</div>
+			</div>
 			<Outlet />
 		</HelmetProvider>
 	);
